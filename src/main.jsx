28da37ab546f4203ea4891baacfee6ac73b6f551
SoftwareDevelopor <saurabhkumar13618@gmail.com>
2025-12-15 20:58:57 +0530

    upload

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 createRoot(document.getElementById('root')).render(
@@ -16,7 +15,3 @@ createRoot(document.getElementById('root')).render(
 )
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
