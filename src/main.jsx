4f399357cec38789f9eaff6000ca98b701ac183c
SoftwareDevelopor <saurabhkumar13618@gmail.com>
2025-12-18 10:08:42 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,27 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
-import { registerSW } from 'virtual:pwa-register'
-
-
-registerSW({
-  onNeedRefresh(){
-    if(confirm("New version available. Do you want to update?")){
-      window.location.reload();
-    }
-  },
-  onOfflineReady() {}
-})
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the default small bar
   e.preventDefault();
-  
-  // Immediately show the large native install prompt
-  // Note: This usually requires a user click elsewhere on the page first 
-  // due to browser security policies.
   e.prompt(); 
-  
   e.userChoice.then((choiceResult) => {
     if (choiceResult.outcome === 'accepted') {
       console.log('User accepted the install');
